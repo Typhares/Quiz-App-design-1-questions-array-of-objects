ed7c5ffe57db41b103f4642f763e035814fe5645
Elizabeth Hall <elizabethhall205@gmail.com>
2018-10-07 09:10:53 -0400

    fixed handSubmit function

--- index.js
+++ index.js
@@ -1,59 +1,8 @@
-let score= 0;
-let currentQuestion= 0; // keeps track of what question we are on
-let questionsArray = [  // the array of questions each question is an Object with key value pairs or `keys`
-  { 
-   title: "Question 1",
-   answers: [ "first", "second", "third", "fourth"],
-   correct: 0
-  }, 
-
- { 
-   title: "Question 2",
-   answers: [ "first", "second", "third", "fourth"],
-   correct: 1 //index of correct answer
-  }, 
 
 
- { 
-   title: "Question 3",
-   answers: [ "first", "second", "third", "fourth"],
-   correct: 2 //index of correct answer
-  }, 
-
-
- { 
-   title: "Question 4",
-   answers: [ "first", "second", "third", "fourth"],
-   correct: 3 //index of correct answer
-  }, 
-    
-];
-//event listeners
-
-$(document).ready(function(){
-
-    $('.start a').click(function(element){
-        element.preventDefault();
-        $('.start').hide();
-        $('.quiz').show();   
-        showQuestion(); //calling line 47 showQuestion
-    });
-    // for giggles and grins, the.append method methodinserts html code 
-        $('ul').append('<li> added choice .append() </li>');
-    
-});
-//these functions will run each time the player clicks on a question title and renders HTML elements to the page
-
-function showQuestion() {
-    let question = questionsArray[currentQuestion]; 
-    //question.title will access the first Key from the Questions Objects in the Array 
-    $('.quiz h2').text(question.title);//method to select h2 and access the Object Key
-    $('.quiz ul').html(``) //makes other questions go away
-    $('.quiz ul').append(`<li> ${question.title} </li>`) //.append the list of choices with the question title and render it to the page
-        console.log(question);
-   //questionsArray[i].title I dont' know how to access the answers
-
-}
+let currentQuestion = 0; 
+let totalNumberOfCorrectAnswers = 0;
+let totalNumberOfQuestions = questionsArray.length;
 
 function checkAnswer() {
 
@@ -62,6 +11,29 @@ function checkAnswer() {
 function showSummary() {
 
 }
+
+function showQuestion() {
+    let question = questionsArray[currentQuestion]; 
+    //question.title will access the first Key from the Questions Objects stored in the state var questionsArray
+    /*for loop here questionsArray[i] will be the correct answer
+    iterate through the questionsArray to access the 'questions' key at i
+    */
+}
+
+/*--- Step 3 - Using functions 
+
+revisit unit 3 architecting client-side apps, spaghetti code
+document.ready
+refer to Felicia's JSFiddle 
+
+
+
+Delete this entire document ready function and replace with $(init) and then write a function to handle all events
+
+    $(document).ready(function(){  //this is an intiatialization function
+        //can only call 1 function, write a function init then make calls to 
+        //add all event listeners to the init function 
+        //shopping list app last form
 
 
 
